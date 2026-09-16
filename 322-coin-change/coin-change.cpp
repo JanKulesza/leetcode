82def92d5c0951301a1de5edc15ffa29a1a64099
Jan Kulesza <151505076+JanKulesza@users.noreply.github.com>
2026-09-16 14:04:33 +0200

    Time: 11 ms (98.46%) | Memory: 15.8 MB (95.59%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -4,9 +4,10 @@ public:
         vector<uint16_t> dp(amount + 1, UINT16_MAX - 1);
         dp[0] = 0;
         for (const int& coin : coins)
-        	for (int j = coin; j <= amount; j++) 
-                if(dp[j] > 1 + dp[j - coin])
-        		    dp[j] = 1 + dp[j - coin];
+            if(coin <= UINT16_MAX)
+            	for (uint16_t j = coin; j <= amount; j++) 
+                    if(dp[j] > 1 + dp[j - coin])
+            		    dp[j] = 1 + dp[j - coin];
 
         return dp.back() == UINT16_MAX - 1 ? -1 : dp.back();
     }
