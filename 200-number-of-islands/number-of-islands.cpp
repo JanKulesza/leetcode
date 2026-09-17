993db5fa7901576a2f51f584ec5b94d09882e834
Jan Kulesza <151505076+JanKulesza@users.noreply.github.com>
2026-09-17 09:34:49 +0200

    Time: 24 ms (77.03%) | Memory: 22.9 MB (32.04%) - LeetSync

---
+++ 200-number-of-islands/number-of-islands.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int numOfIslands = 0;
+        for (int i = 0; i < grid.size(); i++) 
+            for (int j = 0; j < grid[0].size(); j++) {
+                if (grid[i][j] == '1') {
+                    stack<pair<int, int>> stack;
+                    stack.emplace(i, j);
+                    numOfIslands++;
+                    while (!stack.empty())
+                    {
+                        auto [ci, cj] = stack.top();
+                        stack.pop();
+                        if (grid[ci][cj] == '0')
+                            continue;
+
+                        grid[ci][cj] = '0';
+                        for (const auto& [di, dj] : { pair{1,0}, {0,1}, {0,-1}, {-1,0} }) {
+                            if (ci + di < grid.size() && ci + di >= 0 && cj + dj < grid[0].size() && cj + dj >= 0 && grid[ci + di][cj + dj] == '1')
+                                stack.emplace(ci + di, cj + dj);
+                        }
+                    }
+                }
+            }
+        return numOfIslands;
+    }
+};
