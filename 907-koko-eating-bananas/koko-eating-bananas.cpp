45e55efcc371b6b8572f295dd98d684c30c3ccef
Jan Kulesza <151505076+JanKulesza@users.noreply.github.com>
2026-09-17 20:36:22 +0200

    Time: 4 ms (86.45%) | Memory: 23 MB (24.48%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,30 +1,29 @@
 class Solution {
 public:
     int minEatingSpeed(vector<int>& piles, int h) {
-        int r = 0;
-        for (int i = 0; i < piles.size(); i++)
-            if(piles[i] > r)
-        	    r = piles[i];
-        if (piles.size() == h)
-        	return r;
+        int maxi = 0;
+for (int i = 0; i < piles.size(); i++)
+	maxi = max(piles[i], maxi);
+if (piles.size() == h)
+	return maxi;
 
-        int l = 0;
-        r--;
-        while (l < r)
-        {
-        	int k = (l + r) / 2;
-        	int copyH = h;
-        	for (const auto& p : piles)
-        	{
-        		copyH -= p / (k + 1);
-        		if (p % (k + 1) != 0)
-        			copyH--;
-        	}
-        	if (copyH >= 0)
-        		r = k;
-        	else
-        		l = k + 1;
-        }
-        return l + 1;
+int l = 0;
+int r = maxi - 1;
+while (l < r)
+{
+	int k = (l + r) / 2;
+	int copyH = h;
+	for (const auto& p : piles)
+	{
+		copyH -= p / (k + 1);
+		if (p % (k + 1) != 0)
+			copyH--;
+	}
+	if (copyH >= 0)
+		r = k;
+	else
+		l = k + 1;
+}
+return l + 1;
     }
 };
