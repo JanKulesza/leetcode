8ca7b4e2c53156b41d611aaf13e3f881c42653db
Jan Kulesza <151505076+JanKulesza@users.noreply.github.com>
2026-09-19 08:23:58 +0200

    Time: 0 ms (100.00%) | Memory: 9.9 MB (97.44%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -3,15 +3,11 @@ public:
     int rob(vector<int>& nums) {
         if (nums.size() == 1)
             return nums[0];
-        uint16_t prev = nums[0];
-        uint16_t curr = max(nums[0], nums[1]);
-        for (uint8_t i = 2; i < nums.size(); i++)
+        nums[1] = max(nums[0], nums[1]);
+        for (int i = 2; i < nums.size(); i++)
         {
-            uint16_t temp = curr;
-            if(nums[i] + prev > curr)
-                curr = nums[i] + prev;
-            prev = temp;
+            nums[i] = max(nums[i] + nums[i - 2], nums[i - 1]);
         }
-        return curr;
+        return nums.back();
     }
 };
