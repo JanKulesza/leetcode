fd3c36dafe54de911945866821c39cdab6d7ecba
Jan Kulesza <151505076+JanKulesza@users.noreply.github.com>
2026-09-24 11:25:49 +0200

    Time: 67 ms (18.22%) | Memory: 21.9 MB (14.95%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        vector<int> dp(nums.size(), INT_MAX);
+        dp[0] = 0;
+        for(int i = 0; i < nums.size() - 1; i++) {
+            int j = i + 1;
+            while(j <= i + nums[i]) {
+                if(j == nums.size() - 1)
+                    return dp[i] + 1;
+                dp[j] = min(dp[j], dp[i] + 1);
+                j++;
+            }
+        }
+        return dp.back();
+    }
+};
